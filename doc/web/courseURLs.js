abc97e18bc306f2829ef57fb746a010203e8baa8
Christian Forssén <christian.forssen@chalmers.se>
2019-08-12 14:09:32 +0200

    course URLs as javascript variables. Used in html files.

---
+++ doc/web/courseURLs.js
@@ -0,0 +1,4 @@
+var kursmaterial = "https://chalmers.instructure.com/courses/7435/external_tools/214";
+var syllabus = "https://chalmers.instructure.com/courses/7435/assignments/syllabus";
+var veckoplanering = "https://chalmers.instructure.com/courses/7435/external_tools/212";
+var datorlab1 = "https://chalmers.instructure.com/courses/7435/assignments/4895";
