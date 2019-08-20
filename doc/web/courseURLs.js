74550620769ba9186222fb910fba80faf8ba1995
Christian Forssén <christian.forssen@chalmers.se>
2019-08-20 11:19:06 +0200

    javascript creates links depending on if the current page is within an iFrame or not.

--- doc/web/courseURLs.js
+++ doc/web/courseURLs.js
@@ -1,4 +1,36 @@
-var kursmaterial = "https://chalmers.instructure.com/courses/7435/external_tools/214";
-var syllabus = "https://chalmers.instructure.com/courses/7435/assignments/syllabus";
-var veckoplanering = "https://chalmers.instructure.com/courses/7435/external_tools/212";
+// Returns true if the current page is within an iFrame.
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+// course-specific URL
+var courseURL = "https://chalmers.instructure.com/courses/7435/";
+
+// Course-specific url if the current page is within an iFrame.
+// Relative link to file if the current page is the top window
+var kursmaterial = "";
+if (inIframe()) {
+	kursmaterial = courseURL + "external_tools/214";
+} else {
+	kursmaterial = 'material.html';
+}
+
+var syllabus = "";
+if (inIframe()) {
+	syllabus = courseURL + "assignments/syllabus";
+} else {
+	syllabus = 'syllabus.html';
+}
+
+var veckoplanering = "";
+if (inIframe()) {
+	veckoplanering = courseURL + "external_tools/212";
+} else {
+	veckoplanering = 'schedule.html';
+}
+
 var datorlab1 = "https://chalmers.instructure.com/courses/7435/assignments/4895";
