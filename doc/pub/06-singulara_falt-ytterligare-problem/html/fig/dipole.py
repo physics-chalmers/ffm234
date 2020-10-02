8610986d26853327bab5fb5eca39aed95cb92342
Christian Forssén <christian.forssen@chalmers.se>
2020-10-02 16:50:48 +0200

    updated kap 6 lecture notes

--- doc/pub/06-singulara_falt-ytterligare-problem/html/fig/dipole.py
+++ doc/pub/06-singulara_falt-ytterligare-problem/html/fig/dipole.py
@@ -3,7 +3,7 @@ import numpy as np
 import pylab as p
 
 # Make an x,z grid
-grid = np.linspace(-6,6,201)
+grid = np.linspace(-6,6,200)
 x, z = np.meshgrid(grid,grid)
 mu=4*np.pi  # dipole strength (in z-direction)
 # and the 2D cross section of the potential (scalar field)
@@ -15,24 +15,18 @@ from matplotlib import colors
 p.figure(figsize=(6,6))
 heights = np.linspace(-0.6,0.6,7)
 CS = p.contour(x,z,phi,heights,colors='k')
-p.clabel(CS, inline=1, fontsize=10)
+p.clabel(CS, inline=1, fontsize=10,fmt='%3.1f')
 p.xlabel(r'$x$')
 p.ylabel(r'$z$')
 # End contour plot
-p.savefig('dipole.png')
 
-# Start gradient plot
-#--p.figure(figsize=(6,6))
-#--grid_course =  np.linspace(-10000,10000,11)
-#--x_c, y_c = np.meshgrid(grid_course,grid_course)
-#--radius = 2000
-#--outside = x_c**2 + y_c**2 > radius**2
-#--gradh_x = - 2*h0 / (R0**2 * (1 + (x_c**2+y_c**2)/R0**2)**2) * x_c
-#--gradh_y = - 2*h0 / (R0**2 * (1 + (x_c**2+y_c**2)/R0**2)**2) * y_c
-#--p.quiver(x_c[outside]/1000,y_c[outside]/1000,gradh_x[outside],gradh_y[outside],scale=3)
-#--p.xlabel(r'$x$ (km)')
-#--p.ylabel(r'$y$ (km)')
-#--# End gradient plot
-#--p.savefig('hojd_gradient.png')
+# Start fieldline plot
+Fx = mu / (4 * np.pi * (x**2+z**2)**(5/2)) * 2*x*z
+Fz = mu / (4 * np.pi * (x**2+z**2)**(5/2)) * (z**2-x**2)
+p.streamplot(x,z,Fx,Fz)
+# End fieldline plot
+CS = p.contour(x,z,phi,heights,colors='k')
+p.clabel(CS, inline=1, fontsize=10,fmt='%3.1f')
+p.savefig('dipole_fieldplot.png')
 
 #p.show()
